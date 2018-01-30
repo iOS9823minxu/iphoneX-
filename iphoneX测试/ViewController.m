01feb1da8cc313587ed2a50019fa7ee769d71d84
xumin0034315 <yufu@yufudeMacBook-Pro3.local>
2018-01-30 11:14:01 +0800

    three the

--- iphoneX测试/ViewController.m
+++ iphoneX测试/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.title=@"进入首页"; self.navigationController.navigationBar.backgroundColor=[UIColor greenColor];
+    
+    
     self.view.backgroundColor=[UIColor redColor];
 }
 
