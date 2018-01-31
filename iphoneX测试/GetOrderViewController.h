6eeb064d0d61edf7ff41e562bb3b2b1242f11862
xumin0034315 <yufu@yufudeMacBook-Pro3.local>
2018-01-31 09:52:41 +0800

    xxx

---
+++ iphoneX测试/GetOrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  GetOrderViewController.h
+//  iphoneX测试
+//
+//  Created by yufu on 2018/1/30.
+//  Copyright © 2018年 yufu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GetOrderViewController : UIViewController
+
+@end
---
+++ iphoneX测试/GetOrderViewController.m
@@ -0,0 +1,13 @@
+//
+//  GetOrderViewController.m
+//  iphoneX测试
+//
+//  Created by yufu on 2018/1/30.
+//  Copyright © 2018年 yufu. All rights reserved.
+//
+
+#import "GetOrderViewController.h"
+
+@implementation GetOrderViewController
+
+@end
