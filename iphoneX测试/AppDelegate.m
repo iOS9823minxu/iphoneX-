a4984fb8d62ae16221a3ab7e9ea2b82be8831f2b
xumin0034315 <yufu@yufudeMacBook-Pro3.local>
2018-01-31 15:14:05 +0800

    cc

--- iphoneX测试/AppDelegate.m
+++ iphoneX测试/AppDelegate.m
@@ -95,8 +95,10 @@
     if ([context hasChanges] && ![context save:&error]) {
         // Replace this implementation with code to handle the error appropriately.
         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+       
         abort();
+        
+        
     }
 }
 
