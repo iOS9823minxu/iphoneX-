8a6c097c2f6200c144fa2f65645c7e3acb717be0
xumin0034315 <yufu@yufudeMacBook-Pro3.local>
2018-01-31 15:43:30 +0800

    dddd

--- iphoneX测试/AppDelegate.m
+++ iphoneX测试/AppDelegate.m
@@ -93,8 +93,7 @@
     NSManagedObjectContext *context = self.persistentContainer.viewContext;
     NSError *error = nil;
     if ([context hasChanges] && ![context save:&error]) {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+       
        
         abort();
         
